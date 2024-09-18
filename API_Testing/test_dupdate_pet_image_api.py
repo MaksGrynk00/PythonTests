b6bc81d879d0f9e353b63b7aa14987b15bc568c0
MaksGrynk00 <maksym_grynkiv@epam.com>
2024-09-18 15:07:18 +0300

    fix API test ignored

--- API_Testing/test_dupdate_pet_image_api.py
+++ API_Testing/test_dupdate_pet_image_api.py
@@ -1,9 +1,9 @@
 import pytest
-import httpx
-"""	Verify that allows updating Pet’s image """
+import requests
 
-@pytest.mark.asyncio
-async def test_update_pet_image(api_url6, image_path):
+"""Verify that allows updating Pet’s image """
+
+def test_update_pet_image(api_url6, image_path):
     # Open the image file in binary mode
     with open(image_path, "rb") as image_file:
         image_data = image_file.read()
@@ -12,9 +12,8 @@ async def test_update_pet_image(api_url6, image_path):
     data = {
         "file": (image_path, image_data, "image/jpg")
     }
-    # Use httpx to send the POST request with multipart/form-data
-    async with httpx.AsyncClient() as client:
-        response = await client.post(api_url6, files=data)
+    # Use requests to send the POST request with multipart/form-data
+    response = requests.post(api_url6, files=data)
 
     # Assert successful status code (200)
     assert response.status_code == 200
